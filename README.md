1410f10ede4b3a8f20a5c3a38ac475dd1f48d1f4
elsecaller121 <samuel.lu361@gmail.com>
2022-06-16 23:48:34 -0700

    changed argparse to user input; added search by activity name; modifed README to reflect proper dictionary initialization

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Python script that automatically reserves slots for activities in JWC (e.g. rock
 
 To automate the process of "signing-in" without command line input:
     create a Python file "userInfo.py" and create a dictonary in the form
-    credentials = ["username": userString, "password": passString]
+    credentials = {"username": userString, "password": passString}
--- main.py
+++ main.py
@@ -1,51 +1,53 @@
-from modulefinder import Module
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-import argparse
-from getpass import getpass
 
+#gets credentials from userInfo.py if it exists, else get credentials from user input
 try:
     import userInfo
-except ModuleNotFoundError:
-    pass
-
-parser = argparse.ArgumentParser(description="Logging into UCLA networks")
-parser.add_argument("username")
-args = parser.parse_args()
-args.password = getpass("Please enter your UCLA password: ")
-
-if ('userInfo' not in locals()):
-    credentials = args.__dict__
-else:
     credentials = userInfo.credentials
+except ModuleNotFoundError:
+    credentials = dict()
+    credentials["username"] = input('Username: ')
+    credentials["password"] = input('Password: ')
+
 
 if __name__ == "__main__":
+    activity = input('Activity (jwc rock wall, badminton): ')
+
+    #initiates chrome driver and goes to ucla rec website
     URL = "https://secure.recreation.ucla.edu/Program/GetProducts"
     driver = webdriver.Chrome()
+    driver.implicitly_wait(20)
     driver.get(URL)
 
+    #chooses sso to login
     login = driver.find_element(By.ID, "loginLink")
     login.click()
-    driver.implicitly_wait(1)
     sso = driver.find_element(By.XPATH, "//div[@id='section-sign-in-first']//button[@title='CLICK HERE FOR UCLA LOGON']")
-    driver.implicitly_wait(5)
-   
-    time.sleep(2)
+    
+    time.sleep(1)
    
     sso.click()
    
     time.sleep(1)
     
+    #logs in with credentials
     driver.find_element(By.ID, "logon").send_keys(credentials["username"])
     driver.find_element(By.ID, "pass").send_keys(credentials["password"])
 
     driver.find_element(By.NAME, "_eventId_proceed").click()
 
+    #searches for activity
+    search = driver.find_element(By.ID, 'txtSearch')
+    search.send_keys(activity)
+    search.send_keys(Keys.ENTER)
+
     time.sleep(1)
 
-    all_classifications = driver.find_element(By.ID, "00000000-0000-0000-0000-000000000000")
-    all_classifications.click()
-    time.sleep(20)
+    #clicks on activity
+    driver.find_element(By.ID, 'list-group').click()
+
+    time.sleep(1000)
     driver.quit()
